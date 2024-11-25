f6cf177602e161db132015a39ece5e5467fdd5ac
Wilifrisquis <hugocruzok@gmail.com>
2024-11-25 18:50:48 +0100

    Tercer Commit Master

--- src/Practica1.java
+++ src/Practica1.java
@@ -2,6 +2,7 @@ public class Practica1 {
     public static void main(String[] args) {
         System.out.println("Hola buenas tardes");
         System.out.println("Buenas noches");
+        System.out.println("Hola Mundo");
 
     }
 }

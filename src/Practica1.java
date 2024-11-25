402adcedc44fb65e8aa49dae4490ed912ade167a
Wilifrisquis <hugocruzok@gmail.com>
2024-11-25 18:56:45 +0100

    Primer commit segunda rama

--- src/Practica1.java
+++ src/Practica1.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Practica1 {
     public static void main(String[] args) {
         System.out.println("Hola buenas tardes");
         System.out.println("Buenas noches");
         System.out.println("Hola Mundo");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un número");
+        int num1 = sc.nextInt();
 
     }
 }
